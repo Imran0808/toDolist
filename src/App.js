ea1a25c06b64545f882dce975189fb0541917b78
Imran0808 <114299497+Imran0808@users.noreply.github.com>
2023-03-09 10:25:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './component/Header';
+import Home from './component/Home'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Header.css";
+function Header() {
+  return (
+   <nav>TODO List</nav>
+  )
+}
+
+export default Header
